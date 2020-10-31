390e6b93c0ed6db97f29d8b915eeb99f571cceba
mayu <mayu.sidetail0919@gmail.com>
2020-10-31 15:23:46 +0900

    フォームの追加

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("main.html")
+
+@app.route('/result', methods=["POST"])
+def result():
+    text=request.form["input-text"]
+    return render_template("result.html",text=text)
  
 #おまじない
 if __name__ == '__main__':
