0b2d80bd6a1e473793c6d2d7e8ab5c49dee174ca
rin3-jp <20nm704x@vc.ibaraki.ac.jp>
2020-10-31 15:52:24 +0900

    形態素解析用

---
+++ morph/keitaiso.py
@@ -0,0 +1,32 @@
+# 受け取った文章を形態素解析して返す
+# 型は辞書型配列
+# [全文]
+# [[文章1][文章2]]
+# [[[形態素1][形態素2]][[形態素3][形態素4]]]
+# 上記のように返ってくる
+
+import requests
+import json
+
+
+
+appid = "2209ee67bfcb549e49c8f8c08da3f5a837a4085782abe759bafa5de02d7a4dc0"
+request_url = "https://labs.goo.ne.jp/api/morph"
+
+def keitaiso(honbun):
+    req_json = {
+        'app_id' : appid,
+        'sentence' : honbun,
+        'info_filter' : "form|pos"
+    }
+
+    res = requests.post(request_url,json=req_json)
+    res_json = res.json()
+    d = res_json['word_list']
+
+    return d
+
+
+n = input()
+
+print(keitaiso(n))
