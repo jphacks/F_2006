935def2cf7da2b0db74b843e1947e9d123e1bc95
Ysmr-Ry <ryo.yossy@live.jp>
2020-10-31 16:48:43 +0900

    POST implemented

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template("main.html")
 
-@app.route('/result', methods=[POST"])
+@app.route('/result', methods=["POST"])
 def result():
     #if request.headers['Content-Type'] != 'application/json':
      #   print(request.headers['Content-Type'])
@@ -14,13 +14,13 @@ def result():
     data = request.json
     print(data)
     
-    data = data.text
+    data = data['text']
 
     # ここで処理
     data = [data]
 
     data = {
-        text: data
+        'text': data
     }
 
     return jsonify(data)
