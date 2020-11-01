9b4d128bffe82032b3eb2d43f4ac230c3a840b97
Ysmr-Ry <ryo.yossy@live.jp>
2020-11-01 11:38:34 +0900

    baseUrl fixed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html') 
+    return render_template('home.html', baseUrl=request.base_url) 
 
 @app.route('/read')
 def read():
