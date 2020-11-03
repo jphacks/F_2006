1e02995ee40b4b3b17744f8272512cd26618f3a5
asSqr <ryo.yossy@live.jp>
2020-11-03 17:44:41 +0900

    DB 導入 (途中)

---
+++ db/db.ddl
@@ -0,0 +1,19 @@
+drop table if exists t_documents cascade;
+
+create table t_documents (
+  uuid character(36) not null
+  , content text not null
+  , split_uuid character(36) not null
+  , created_at timestamp not null
+  , updated_at timestamp not null
+  , constraint t_documents_PKC primary key (uuid)
+);
+
+drop table if exists t_split_units cascade;
+
+create table t_split_units (
+  uuid character(36) not null
+  , index integer not null
+  , content text not null
+  , constraint t_split_units_PKC primary key (uuid)  
+);
--- main.py
+++ main.py
@@ -2,12 +2,50 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import json
 import morph
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 CORS(app)
+
+db_uri = os.environ.get('DATABASE_URL') or "postgresql://localhost/flash-reading-db"
+app.config = ['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+class tDocuments(db.Model):
+    __tablename__ = 't_documents'
+    uuid = db.Column(db.String(36), primary_key=True)
+    content = db.Column(db.Text, primary_key=False)
+    split_uuid = db.Column(db.String(36), primary_key=False)
+    created_at = db.Column(db.DateTime(), primary_key=True)
+    updated_at = db.Column(db.DateTime(), primary_key=True)
+
+class tSplitUnits(db.Model):
+    __tablename__ = 't_split_units'
+    uuid = db.Column(db.String(36), primary_key=True)
+    index = db.Column(db,Integer, primary_key=False)
+    content = db.Column(db.Text, primary_key=False)
 
 @app.route('/')
 def home():
     return render_template('home.html', baseUrl=request.base_url) 
+
+@app.route('/list')
+def list():
+    docs = tDocuments.query.all()
+
+    for doc in docs:
+        print(doc)
+
+        units = tSplitUnits
+            .query
+            .get(doc.split_uuid)
+            .order_by(tSplitUnits.index)
+
+        print(units)
+        
+        doc.units = units
+
+    return render_template('list.html', baseUrl=request.base_url, docList=doc) 
 
 @app.route('/read')
 def read():
