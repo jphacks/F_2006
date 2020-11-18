e5be0040fff91007da1a7f44366bf20657613991
Electronical-ARAI-san <61216147+H-J-Ainashi@users.noreply.github.com>
2020-11-18 21:35:07 +0900

    長文時はクリップボードにコピーを行う実装

--- extensions/chrome/background_chrome.js
+++ extensions/chrome/background_chrome.js
@@ -7,8 +7,26 @@ chrome.contextMenus.create({
   onclick: function (info) {
     const sentence = info.selectionText;
 
-    const url = 'http://flash-reading-dev.herokuapp.com/read?q=' + encodeURI(sentence);//'https://flash-reading.herokuapp.com/read';
+    let url = 'http://flash-reading-dev.herokuapp.com/read?q=' + encodeURI(sentence);//'https://flash-reading.herokuapp.com/read';
 
+    // URLが長すぎる場合
+    if (url.length > 2047){
+      const result = window.confirm("文章が長いので直接リンクを読み込むことができません。\nクリップボードにコピーしたうえで続行しますか。\n（新しいタブで開くのでテキストボックスに貼り付けてください。）");
+      if (result == true){
+        // クリップボードにコピー
+        const textArea = document.createElement("textarea");
+        document.body.appendChild(textArea);
+        textArea.value = sentence;
+        textArea.select();
+        document.execCommand("copy");
+
+        document.body.removeChild(textArea);
+        url = "http://flash-reading-dev.herokuapp.com/read";
+      }
+      else
+        return;
+
+    }
     window.open(url, 'newtab');
-  }
+    }
 })
